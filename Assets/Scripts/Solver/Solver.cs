06394d144f1ab15314ac75fd3c98c7b1e85890a9
Mário Pinto <mariojgpinto@gmail.com>
2015-12-02 00:41:21 +0000

    Solver
    Added Solution with memory problems

--- Assets/Scripts/Solver/Solver.cs
+++ Assets/Scripts/Solver/Solver.cs
@@ -1,182 +1,144 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-
-public class SolverBoard{
-	public bool[] matrix;
-	public int width, height;
-	public int minX, maxX, minY, maxY;
-
-	bool solved = false;
-
-	public SolverBoard(string _matrix){
-		matrix = new bool[_matrix.Length];
-		for(int i = 0 ; i < _matrix.Length ; matrix[i]=_matrix[i++] == '1');
-
-		width = height = Mathf.CeilToInt(Mathf.Sqrt(_matrix.Length));
-
-		minX = 0;
-		minY = 0;
-		maxX = width;
-		maxY = height;
-
-		UpdateLimits();
-	}
-
-	public SolverBoard(SolverBoard _board){
-		matrix = new bool[_board.matrix.Length];
-		for(int i = 0 ; i < _board.matrix.Length ; matrix[i]=_board.matrix[i++]);
-
-		width = height = Mathf.CeilToInt(Mathf.Sqrt(matrix.Length));
-
-		UpdateLimits();
-	}
-
-	public void ApplyMove(Vector2 move){
-		int x = (int)move.x;
-		int y = (int)move.y;
-
-		int idx = y * width + x;
-		
-		matrix[idx] = !matrix[idx];
-		for (int i = minX; i < maxX; ++i) {
-			idx = width * y + i;
-			
-			matrix[idx] = !matrix[idx];
-			
-		}
-		
-		for (int j = minY; j < maxY; ++j) {
-			idx = width * j + x;
-			matrix[idx] = !matrix[idx];
-		}
-
-		UpdateLimits();
-	}
-
-	public void UpdateLimits(){
-		bool foundMinX = false;
-		for(int x = minX ; x < maxX && !foundMinX ; ++x){
-			for(int y = minY ; y < maxY && !foundMinX ; ++y){
-				if(matrix[width * y + x]){
-					minX = x;
-					foundMinX = true;
-				}
-			}
-		}
-		if(!foundMinX) {
-			minX = maxY;
-			solved = true;
-			return;
-		}
-
-		bool foundMinY = false;
-		for(int y = minY ; y < maxY && !foundMinY ; ++y){
-			for(int x = minX ; x < maxX && !foundMinY ; ++x){
-				if(matrix[width * y + x]){
-					minY = y;
-					foundMinY = true;
-				}
-			}
-		}
-		if(!foundMinY) {
-			minY = maxY;
-			solved = true;
-			return;
-		}
-
-		bool foundMaxX = false;
-		for(int x = maxX-1 ; x >= minX && !foundMaxX ; --x){
-			for(int y = minY ; y < maxY && !foundMaxX ; ++y){
-				if(matrix[width * y + x]){
-					maxX = x + 1;
-					foundMaxX = true;
-				}
-			}
-		}
-		if(!foundMaxX) {
-			maxX = minX;
-			solved = true;
-			return;
-		}
-
-		bool foundMaxY = false;
-		for(int y = maxY-1 ; y >= minY && !foundMaxY ; --y){
-			for(int x = minX ; x < maxX && !foundMaxY ; ++x){
-				if(matrix[width * y + x]){
-					maxY = y + 1;
-					foundMaxY = true;
-				}
-			}
-		}
-		if(!foundMaxY) {
-			maxY = minY;
-			solved = true;
-			return;
-		}
-	}
-
-	public string ToString(){
-		string str = "";
-	
-		str = "Board (" + width + "," + height + "):\n";
-
-		for(int j = height-1 ; j >= 0 ; --j){
-			for(int i = 0 ; i < width ; ++i){
-				str += (matrix[j*width + i] ? "1" : "0") + ",";
-			}
-			str += "\n";
-		}
-
-		str += "BordersX[" + minX + "," + maxX + "]\n";
-		str += "BordersY[" + minY + "," + maxY + "]\n";
-
-		return str;
-	}
+public class BoardState{
+	public Vector2 move;
+	public SolverBoard board;
+	public int parent;
 }
 
-public struct State {
-    public byte[] currentMatrix;
-    public byte[] previousMatrix;
-
-    public Vector2 move;
-}
 public class Solver : MonoBehaviour {
     string matrix;
     int length;
 
 	public int minMoves = -1;
-	public List<Vector2> moves = null;
+
+	bool solved = false;
+	int nextLevel = 1;
+	int currentLevel = 0;
+	List<List<BoardState>> statesByLevel = new List<List<BoardState>>();
 
 	public Solver(string _matrix){
 		matrix = _matrix;
 		length = Mathf.CeilToInt(Mathf.Sqrt(_matrix.Length));
 	}
 
+	void AddLevel(){
+		statesByLevel.Add(new List<BoardState>());
+		currentLevel++;
+		nextLevel++;
+	}
+
+	void AddBoardToSolve(SolverBoard b, int x, int y, int _parent){
+		statesByLevel[nextLevel].Add(new BoardState{
+			move = new Vector2(x,y), 
+			board = b,
+			parent = _parent
+		});
+	}
+
     public void Solve() {
 		SolverBoard board = new SolverBoard(matrix);
 
-        for(int i = 0; i < length; ++i) {
-            for(int j = 0; j < length; ++j) {
+		statesByLevel = new List<List<BoardState>>();
+		statesByLevel.Add(new List<BoardState>());
+		statesByLevel.Add(new List<BoardState>());
+		nextLevel = 1;
+		currentLevel = 0;
 
-            }
-        }
+		statesByLevel[currentLevel].Add(new BoardState{
+			move = new Vector2(0,0), 
+			board = board,
+			parent = -1				
+		});
+
+		while(!solved){
+			for(int b = 0 ; b < statesByLevel[currentLevel].Count ; ++b){
+				SolverBoard currentBoard = statesByLevel[currentLevel][b].board;
+
+				for(int x = currentBoard.minX; x < currentBoard.maxX; ++x) {
+					for(int y = currentBoard.minY; y < currentBoard.maxY; ++y) {
+						SolverBoard aux = new SolverBoard(currentBoard);
+						
+						aux.ApplyMove(x,y);
+
+						if(statesByLevel.Count > 2){
+							//int grandFather = statesByLevel[currentLevel][b].parent
+							bool exist = false;
+							for(int i = 0 ; i < statesByLevel.Count-1 && !exist ; ++i){
+								for(int j = 0 ; j < statesByLevel[i].Count && !exist ; ++j){
+									if(SolverBoard.Compare(aux, statesByLevel[i][j].board))
+										exist = true;									
+								}
+							}
+
+							if(!exist)
+								AddBoardToSolve(aux, x, y, b);
+						}
+						else{
+							AddBoardToSolve(aux, x, y, b);
+						}
+
+//						AddBoardToSolve(aux, x, y, b);
+
+						if(aux.solved){
+							//PUZZLE SOLVED!!!
+							solved = true;
+						}
+					}
+				}
+			}
+
+			if(!solved){				
+				AddLevel();
+			}
+		}
+
+		minMoves = statesByLevel.Count -1;
+
+		int nSolutions = 0; 
+		for(int i = 0 ; i < statesByLevel[minMoves].Count ; ++i){
+			if(statesByLevel[minMoves][i].board.solved){
+				nSolutions++;
+
+				BoardState b = statesByLevel[minMoves][i];
+				int level = minMoves;
+				string solution = "";
+				while (b.parent != -1){
+					solution = "(" + b.move.x + "," + b.move.y + ")" + solution;
+					level--;
+					b = statesByLevel[level][b.parent];
+				}
+
+				Debug.Log("Solution " + (nSolutions+1) + ": " + solution);
+			}
+		}
+
+		Debug.Log("Min Moves: " + minMoves) ;
+		Debug.Log("NSolutions: " + nSolutions) ;
 
 //        _min = 0;
 //        _moves = "";
     }
 
+	public void PrintResult(){
 
+	}
     
     // Use this for initialization
 	void Start () {
-		SolverBoard b = new SolverBoard("000011001");
+//		string m = "001101110010010100101100001000000010";
+		string m = "1110000101100100";
+		SolverBoard b = new SolverBoard(m);
 
 		Debug.Log(b.ToString());
 
-		b.ApplyMove(new Vector2(2,1));
+		matrix = m;
+		length = Mathf.CeilToInt(Mathf.Sqrt(m.Length));
 
-		Debug.Log(b.ToString());
+		statesByLevel = new List<List<BoardState>>();
+
+		Solve();
 	}
 	
 	// Update is called once per frame
